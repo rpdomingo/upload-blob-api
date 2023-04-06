441d5ff08c9f8da0a4cab64f1f8b06d67e694d66
rpdomingo <reynbowdomingo@gmail.com>
2023-04-06 22:55:37 +0800

    remove the uri

--- Program.cs
+++ Program.cs
@@ -13,10 +13,10 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped(_ => {
     return new BlobServiceClient(builder.Configuration.GetConnectionString("AzureBlobStorage"));
 });
-builder.Services.AddScoped(_ => {
-    return new BlobServiceClient(new Uri("https://devuploadblobstorage.blob.core.windows.net"),
-        new DefaultAzureCredential());
-});
+// builder.Services.AddScoped(_ => {
+//     return new BlobServiceClient(new Uri("https://devuploadblobstorage.blob.core.windows.net"),
+//         new DefaultAzureCredential());
+// });
 
 builder.Services.AddScoped<IFileService, FileService>();
 
